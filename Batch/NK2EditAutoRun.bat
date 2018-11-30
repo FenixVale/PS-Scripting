a3d149d6b2706fbd7bbc07d818b52fa733313475
FenixVale <33432475+FenixVale@users.noreply.github.com>
2018-11-30 10:20:22 -0500

    Update and rename NK2EditAutoRun.bat to Batch/NK2EditAutoRun.bat

--- Batch/NK2EditAutoRun.bat
+++ Batch/NK2EditAutoRun.bat
@@ -1,5 +1,5 @@
 @ECHO off
-SET LookForFile="%localappdata%\COMPUTREX\2run.txt"
+SET LookForFile="%localappdata%\NK2\2run.txt"
 
 :CheckForFile
 IF EXIST "%LookForFile%" (
@@ -10,19 +10,19 @@ GOTO RunClearJob
 
 :RunClearJob
 ECHO File not found. Running NK2Edit
-start "" "\\192.168.30.24\Computrex\nk2\nk2script.bat"
+start "" "\\ENTERDIRECTORY\nk2\nk2script.bat"
 ECHO Creating First Run File
-If not exist "%localappdata%\COMPUTREX\" mkdir "%localappdata%\COMPUTREX\"
-echo "First run complete" >> "%localappdata%\COMPUTREX\1run.txt"
+If not exist "%localappdata%\NK2\" mkdir "%localappdata%\NK2\"
+echo "First run complete" >> "%localappdata%\NK2\1run.txt"
 ECHO Launching Outlook
 start "" "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"
 Timeout 5
 ECHO Running NK2Edit again
-Start "" "\\192.168.30.24\Computrex\nk2\nk2script.bat"
+Start "" "\\ENTERDIRECTORYnk2\nk2script.bat"
 ECHO Killing Outlook
 taskkill /IM OUTLOOK.EXE /f
 ECHO Creating Second Run File
-echo "Second run complete" >>"%localappdata%\COMPUTREX\2run.txt"
+echo "Second run complete" >>"%localappdata%\NK2\2run.txt"
 EXIT
 
 
